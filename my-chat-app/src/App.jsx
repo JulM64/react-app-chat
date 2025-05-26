dccc534e571572746820740b13e5e13ecfac5cb6
JulM64 MVENG <julienmveng6@gmail.com>
2025-05-26 09:36:33 +0000

    changing code App.jsx

--- my-chat-app/src/App.jsx
+++ my-chat-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -29,6 +29,11 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
+  )
+}*/
+const App = () =>{
+  return(
+    <div className=''>App</div>
   )
 }
 
--- my-chat-app/vite.config.js
+++ my-chat-app/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // allows external access
+    strictPort: true,
+    port: 5173, // or the port Vite is using
+    origin: 'https://5173-julm64-reactappchat-96awp9li94v.ws-eu119.gitpod.io',
+    hmr: {
+      protocol: 'wss',
+      host: '5173-julm64-reactappchat-96awp9li94v.ws-eu119.gitpod.io',
+    }
+  }
 })
