b92c6b9242f3059a825a6123e084182594e771ae
JulM64 MVENG <julienmveng6@gmail.com>
2025-06-09 10:48:48 +0000

    changing of button plus

--- my-chat-app/src/components/list/chatList/ChatList.jsx
+++ my-chat-app/src/components/list/chatList/ChatList.jsx
@@ -1,5 +1,8 @@
 import "./chatList.css"
+import React, { useState } from 'react';
+
 const ChatList = () => {
+    const [addMode,setAddMode]=useState(false)
     return (
         <div className="chatList">
             <div className="search">
@@ -7,7 +10,9 @@ const ChatList = () => {
                     <img src="./search.png" alt=""/>
                     <input type="text" placeholder="Search"/>
                 </div>
-                <img src="./plus.png" alt="" className="add"/>
+                <img src={addMode ? "./minus.png":"./plus.png"} 
+                alt="" className="add"
+                onClick={() => setAddMode((prev)=>!prev)}/>
             </div>
         </div>
     )
